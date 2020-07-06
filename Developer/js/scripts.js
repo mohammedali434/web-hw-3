178bae4f96ecaedc1ecf8f90717e7607b226ee87
mohammedali434 <66567880+mohammedali434@users.noreply.github.com>
2020-07-06 10:22:59 -0700

    انتهى الواجب

---
+++ Developer/js/scripts.js
@@ -0,0 +1,12 @@
+alert("كم عمرك")
+let age = new Date().getHours();
+if (25>age<40) {
+ console.log(" ناسف على الوظيفة")
+console.log("تم قبولك")
+}
+alert("كم هي عدد الخبرات لديك")
+console.log("6")
+
+
+alert("ماهي لغات البرمجة التي تدقنها")
+console.log("java script")
